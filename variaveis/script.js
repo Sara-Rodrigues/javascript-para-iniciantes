93a7b93d14ae3973aa006b37dfe763ec295d7c70
Sara-Rodrigues <sara.slcr67@gmail.com>
2020-07-22 18:21:27 -0300

    Variavéis

---
+++ variaveis/script.js
@@ -0,0 +1,64 @@
+var nome = 'André';
+let idade = 28;
+const possuiFaculdade = true;
+
+var preco = 20;
+var totalComprado = 5;
+var precoTotal = preco * totalComprado;
+
+
+var nome = 'André';
+var idade = 28;
+var possuiFaculdade = true;
+
+
+var nome = 'André',
+    idade = 28,
+    possuiFaculdade = true;
+
+var precoAplicativo;
+// retorna undefined
+
+console.log(nome);
+var nome = 'André';
+// Retorna undefined
+
+var profissao = 'Designer';
+console.log(profissao);
+// Retornar Designer
+
+
+var idade = 28;
+idade = 29;
+
+let preco = 50;
+preco = 25;
+
+const possuiFaculdade = true;
+possuiFaculdade = false;
+// Retorna um erro
+
+
+
+// Declarar uma variável com o seu nome
+var nome = "Sara";
+
+// Declarar uma variável com a sua idade
+var idade = 24;
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = "Pizza";
+
+// Declarar 5 variáveis diferentes sem valores
+var time, 
+    professor, 
+    pais, 
+    endereco, 
+    rua;
+
+console.log(nome, idade, comidaFavorita);
+
