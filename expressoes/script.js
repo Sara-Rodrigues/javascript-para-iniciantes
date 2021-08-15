ecef8a58424f554f74b46d00db2d63c2f13a9ad4
Sara-Rodrigues <sara.slcr67@gmail.com>
2021-08-15 20:19:52 -0300

    expressoes

---
+++ expressoes/script.js
@@ -0,0 +1,11 @@
+const grupoA = 100;
+const grupoB = 300;
+
+// if ternário
+const vencedor = grupoA > grupoB ? 'Grupo A Venceu' : 'Grupo B Venceu';
+
+const numeros = [2, 3, 4, 5, 6];
+const total = numeros.filter((numero) => numero > 4);
+
+const active = true;
+const button = active && 'Botão está ativo';
